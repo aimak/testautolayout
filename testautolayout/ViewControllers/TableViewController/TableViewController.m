030244cefe94c1d0646e6660112d6ab9ed0aa3a2
Paul Renson <paul.renson@tapptic.com>
2014-03-06 15:21:28 +0100

    Now trying to make Autolayout work for the tableHeaderView of a UITableView. Using this solution : https://github.com/maniak-dobrii/iOS-solutions

--- testautolayout/ViewControllers/TableViewController/TableViewController.m
+++ testautolayout/ViewControllers/TableViewController/TableViewController.m
@@ -10,6 +10,8 @@
 
 @interface TableViewController ()
 @property (strong, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) IBOutlet UIView *tableHeaderWrapperView;
 
 @end
 
@@ -31,6 +33,9 @@
     
     [self.descriptionLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
     self.descriptionLabel.text = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in posuere felis. Maecenas non varius libero, vel accumsan arcu. Maecenas commodo tempor lacus, a pellentesque lorem accumsan sed. Nunc rutrum et nulla vitae varius. Phasellus ullamcorper felis vel adipiscing lacinia. In nisi sem, porta sed lobortis dapibus, malesuada ut nulla.";
 }
 
@@ -40,10 +45,67 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    [self updateTableViewHeaderViewHeight];
+}
+
+/**
+ tableView's tableViewHeaderView contains wrapper view, which height is evaluated
+ with Auto Layout. Here I use evaluated height and update tableView's tableViewHeaderView's frame.
+ 
+ New height for tableViewHeaderView doesn't applies without magic, that's why I call -resetTableViewHeaderView.
+ And again, this doesn't work due to some internals of UITableView, so -resetTableViewHeaderView call is scheduled
+ in the main run loop just after it's the last iteration finishes.
+ */
+- (void)updateTableViewHeaderViewHeight
+{
+    // get height of the wrapper and apply it to a header
+    CGRect Frame = self.tableView.tableHeaderView.frame;
+    Frame.size.height = self.tableHeaderWrapperView.frame.size.height;
+    self.tableView.tableHeaderView.frame = Frame;
+    
+    // this magic applies the above changes
+    // note, that if you won't schedule this call to the next run loop iteration
+    // you'll get and error
+    [self performSelector:@selector(resetTableViewHeaderView) withObject:self afterDelay:0];
+}
+
+// yeah, gues there's something special in the setter
+- (void)resetTableViewHeaderView
+{
+    // whew, this could be animated!
+    
+    self.tableView.tableHeaderView = self.tableView.tableHeaderView;
+    
+    
+}
+
 #pragma mark - actions
 
 - (IBAction)quit:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - TableView
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 50.0f;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell * c = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    return c;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
 
 @end
